8cf5dec29e213c0e24bbae791675bc0406b08170
Felipe <felipe@MacBook-Air-de-Felipe.local>
2022-03-19 07:29:08 -0600

    upload

---
+++ src/main/java/page/InitPage.java
@@ -0,0 +1,18 @@
+package page;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class InitPage {
+    public InitPage(WebDriver webDriver){
+        this.webDriver = webDriver;
+        PageFactory.initElements(this.webDriver,this);
+    }
+    private WebDriver webDriver;
+@FindBy(id = "uno")
+WebElement uno;
+
+public void getUrl(String url){
+    webDriver.get(url);
+}
+}
---
+++ src/test/java/test/FirsTest.java
@@ -0,0 +1,24 @@
+package test;
+import page.InitPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class FirsTest {
+    private WebDriver webDriver;
+@BeforeMethod
+    public void setUp(){
+    System.setProperty("webdriver.gecko.driver","/home/felipe/geckodriver");
+    webDriver = new FirefoxDriver();
+}
+@Test
+    public void simpleTest(){
+    InitPage initPage = new InitPage(webDriver);
+    initPage.getUrl("https://google.com");
+}
+@AfterMethod
+    public void cleanUp(){
+    webDriver.close();
+    webDriver.quit();
+}
+}
