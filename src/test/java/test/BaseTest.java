b17a89bce72b0b3d5e21c6a80c917f8c7d5ddcca
Felipe <felipe@MacBook-Air-de-Felipe.local>
2022-04-03 18:30:17 -0500

    primera prueba con docker

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -2,15 +2,21 @@ package test;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTest {
     protected WebDriver webDriver;
     @BeforeMethod
-    public void setUp(){
+    public void setUp() throws MalformedURLException {
         System.setProperty("webdriver.gecko.driver","/usr/bin/geckodriver");
         //System.setProperty("webdriver.gecko.driver","/Users/felipe/Documents/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
         options.setHeadless(true);
+        webDriver = new RemoteWebDriver(new URL("rewre"),options);
         webDriver = new FirefoxDriver(options);
     }
     public WebDriver getWebDriver(){
